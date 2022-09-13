ceea7713487598430427ea398e7f84896a50b0ba
thecesargamer39 <thecesargamer39@gmail.com>
2022-09-13 17:02:42 -0500

    Array´s

---
+++ Cajero/src/PracticaConStrings.java
@@ -0,0 +1,25 @@
+public class PracticaConStrings {
+    public static void main(String[] args) {
+        String unString = new String("soy un string");
+
+        String otroString = new String(unString);
+
+        String miTERCERsTRING ="ESTE STRING CREA DIRECTAMENTE UNA CADENA DE CARACTERES ENTRECOMILLDA";
+
+
+        var miString = "abcde";
+        char caracter;
+        caracter = miString.charAt(2);
+        System.out.println("el caracter en la posicion 3 es: " + caracter);
+
+        String resuldadoConCat = miString.concat("fghij");
+        System.out.println("el resultado de la concatenacion es: "+resuldadoConCat);
+
+        String Concatena
+
+
+
+
+
+    }
+}
